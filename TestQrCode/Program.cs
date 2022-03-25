d73244f70ce199470cc69107a80b54e3b3eedb81
ststeiger <quandary82@hailmail.net>
2022-03-25 14:10:08 +0100

    Added resx serialization

--- TestQrCode/Program.cs
+++ TestQrCode/Program.cs
@@ -3,9 +3,9 @@ namespace TestQrCode
 {
 
 
+
     static class Program
     {
-
 
         public static void ChangeNamespaces()
         {
@@ -100,6 +100,62 @@ namespace TestQrCode
         // C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\Common7\IDE\PublicAssemblies
 
 
+        public class ResourceNode 
+        {
+            public string Value;
+            public string Comment;
+        }
+
+
+        public static string ReadResourceFile()
+        {
+            string[] languages = new string[] { "de", "fr", "it", "en" };
+            string pathPattern = System.AppDomain.CurrentDomain.BaseDirectory;
+            pathPattern = System.IO.Path.Combine(pathPattern, "..", "..", "..", "libQrCodeGenerator", "Resources", "QRBillText-{0}.resx");
+            pathPattern = System.IO.Path.GetFullPath(pathPattern);
+
+            System.Collections.Generic.Dictionary<string, System.Collections.Generic.Dictionary<string, string>> dict = new System.Collections.Generic.Dictionary<string, System.Collections.Generic.Dictionary<string, string>>(System.StringComparer.InvariantCultureIgnoreCase);
+            System.Collections.Generic.Dictionary<string, System.Collections.Generic.Dictionary<string, ResourceNode>> dict2 = new System.Collections.Generic.Dictionary<string, System.Collections.Generic.Dictionary<string, ResourceNode>>(System.StringComparer.InvariantCultureIgnoreCase);
+
+            foreach (string lang in languages)
+            {
+                dict[lang] = new System.Collections.Generic.Dictionary<string, string>(System.StringComparer.InvariantCultureIgnoreCase);
+                dict2[lang] = new System.Collections.Generic.Dictionary<string, ResourceNode>(System.StringComparer.InvariantCultureIgnoreCase);
+                
+                string file = string.Format(pathPattern, lang);
+                System.Resources.ResXResourceReader rr = new System.Resources.ResXResourceReader(file);
+                rr.UseResXDataNodes = true;
+
+                foreach (System.Collections.DictionaryEntry entry in rr)
+                {
+                    System.Resources.ResXDataNode node = (System.Resources.ResXDataNode)entry.Value;
+                    string value = (string) node.GetValue((System.ComponentModel.Design.ITypeResolutionService)null);
+                    string comment = node.Comment;
+
+                    System.Console.WriteLine(value);
+                    if (!string.IsNullOrEmpty(comment))
+                    {
+                        System.Console.Write(" ("); 
+                        System.Console.Write(comment); 
+                        System.Console.WriteLine(")"); 
+                    }
+
+                    // dict[lang][entry.Key.ToString()] = entry.Value.ToString(); // when not using UseResXDataNodes = true
+                    dict[lang][entry.Key.ToString()] = value;
+                    dict2[lang][entry.Key.ToString()] = new ResourceNode() { Value = value, Comment = comment };
+                } // Next entry 
+
+                rr.Close();
+            } // Next lang 
+
+            string json = Newtonsoft.Json.JsonConvert.SerializeObject(dict, Newtonsoft.Json.Formatting.Indented);
+            string json2 = Newtonsoft.Json.JsonConvert.SerializeObject(dict2, Newtonsoft.Json.Formatting.Indented);
+            System.Console.WriteLine(json2);
+
+            return json;
+        } // End Sub ReadResourceFile 
+
+
         /// <summary>
         /// Der Haupteinstiegspunkt für die Anwendung.
         /// </summary>
@@ -112,7 +168,7 @@ namespace TestQrCode
             System.Windows.Forms.Application.SetCompatibleTextRenderingDefault(false);
             System.Windows.Forms.Application.Run(new Form1());
 #endif
-
+            ReadResourceFile();
             string RPT_ES_Zweck = "Rechnungs-Nr. 2022-02-21-133\r\nKunde-Nr. 1234";
 
 
@@ -126,11 +182,12 @@ namespace TestQrCode
 
             libQrCodeGenerator.Tests.Validate();
 
+
             // Address line 2 mandator (Page 29/30 SIX)
             // https://github.com/manuelbl/SwissQRBill/issues/29
             // The “Ultimate creditor” element is intended for use in the future but will not be used
             // when QR - bill is introduced and should therefore not be filled in.
-            byte[] png = GetQrBill("de", "CH4431999123000889012", RPT_Kosten, "CHF"
+            byte[] png = GetQrBill("de", "CH93 0900 0000 8000 0209 2", RPT_Kosten, "CHF"
                  // , "Utopia Planetia AG", "Postfach 1234", "8000 Zürich", "CH"
                  , "Utopia Planetia AG", "Postfach 1234", "", "CH"
                  , rechnungsempfänger, adresse1, plz_ort, country
